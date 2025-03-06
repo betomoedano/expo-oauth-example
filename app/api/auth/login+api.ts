cf1a013013db2a6568376b150c8eba09e298ceaa
betomoedano <betomoedano@outlook.com>
2025-03-05 18:57:51 -0600

    delete comments

--- app/api/auth/login+api.ts
+++ app/api/auth/login+api.ts
@@ -5,11 +5,7 @@ const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const platform = searchParams.get("platform");
-
-  console.log("Login API called");
-  console.log("GOOGLE_CLIENT_ID:", GOOGLE_CLIENT_ID);
-  console.log("GOOGLE_REDIRECT_URI:", GOOGLE_REDIRECT_URI);
-
+  // TODO: Add CSRF protection
   // const state = generateUUID(); // Use a secure state to prevent CSRF
 
   if (!GOOGLE_CLIENT_ID || !GOOGLE_REDIRECT_URI) {
@@ -38,24 +34,4 @@ export async function GET(req: Request) {
   console.log("Redirecting to:", redirectUrl);
 
   return Response.redirect(redirectUrl, 302);
-  // return new Response(null, {
-  //   status: 302,
-  //   headers: {
-  //     Location: redirectUrl,
-  //   },
-  // });
 }
-
-// function generateUUID() {
-//   const arr = new Uint8Array(16);
-//   crypto.getRandomValues(arr);
-//   arr[6] = (arr[6] & 0x0f) | 0x40; // Version 4
-//   arr[8] = (arr[8] & 0x3f) | 0x80; // Variant 10
-//   return [...arr]
-//     .map((b, i) =>
-//       [4, 6, 8, 10].includes(i)
-//         ? `-${b.toString(16).padStart(2, "0")}`
-//         : b.toString(16).padStart(2, "0")
-//     )
-//     .join("");
-// }
